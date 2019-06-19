37b6ede4ede50c2f92c4c61f0a41f144c255f646
Maksim R <cat@mos-it.com>
2019-06-19 23:29:58 +0300

    Testing controllers

--- spec/factories/question.rb
+++ spec/factories/question.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
   factory :question do
     title { 'Question title' }
     text  { 'Question body' }
+
+    trait :invalid do
+      title { nil }
+      text  { nil }
+    end
   end
 end
